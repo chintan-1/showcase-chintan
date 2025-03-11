1e3611484506c46c99de1def65991bcd12b411a2
Chintan Kothari <chintankothari@Chintans-MacBook-Air.local>
2025-03-11 11:33:48 +0530

    removed image from about me section and skills added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, AboutMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+
 }
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,8 +1,9 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.css'
 })
@@ -10,6 +11,6 @@ import { Component } from '@angular/core';
 export class MySkillsComponent {
   skills:string[];
   constructor() {
-    this.skills = ["Django", "Angular" ];
+    this.skills = ["Django", "Angular", "Typescript", "HTML", "CSS", "Javascript", "Python", "SOLID Priciples", "Design Pattern", "Google Cloud Platform (GCP)", "Terraform", "kafka", "Docker(Beginner)", "Kubernetes (Beginner)", "CI/CD Pipeline", "Data Structure and algorithms", "Object-Oriented Programming", "Communication", "Analytical Thinking", "Detail Oriented"];
   }
 }
